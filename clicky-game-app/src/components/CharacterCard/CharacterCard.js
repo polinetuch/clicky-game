03256ea4232cd5c05b91de2756be1f3c1b2e8ce5
Polinet <uchpolinet@gmail.com>
2019-10-21 23:40:05 +1100

    created a CharacterCard function with props as parameter to display all characters

--- clicky-game-app/src/components/CharacterCard/CharacterCard.js
+++ clicky-game-app/src/components/CharacterCard/CharacterCard.js
@@ -1,6 +1,28 @@
 import React from 'react';
-import ReactDom from "react-dom";
-import App from "./App";
 import "./CharacterCard.css";
 
-ReactDom.render(<App />, document.getElementById("root"));
+function CharacterCard(props) {
+    return (
+        <div className="card">
+            <div className="img-container">
+                <img alt={props.name} src={props.image} />
+            </div>
+            <div className="content">
+                <ul>
+                    <li>
+                        <strong>Name</strong> {props.name}
+                    </li>
+                    <li>
+                        <strong>Name</strong> {props.name}
+                    </li>
+                    <li>
+                        <strong>Name</strong> {props.name}
+                    </li>
+                </ul>
+            </div>
+            <span onClick={() => props.shuffleCharacter(props.id)} className="shuffle"></span>
+        </div>
+    );
+}
+
+export default CharacterCard;
