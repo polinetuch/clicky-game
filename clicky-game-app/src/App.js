dbccc07705df5bfd21c1a8f2688399786cd970aa
Polinet <uchpolinet@gmail.com>
2019-10-25 00:54:34 +1100

    all images are now displaying in browser

--- clicky-game-app/src/App.js
+++ clicky-game-app/src/App.js
@@ -1,82 +1,28 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CharacterCard from "./components/CharacterCard/CharacterCard";
+import friends from "./friends.json";
 
 
 class App extends Component {
 
   state = {
-  };
-
-  render() {
-    return (<h1>hello world</h1>)
+    friends
   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // state = {
-  //   characters: characters,
-  //   pickedCharacter: [],
-  //   topScore: 0,
-  //   alertMessage: ""
-  // };
-
-  // handlePicked = event => {
-    
-  //   const name = event.target.attribute.getNamedItem("name").value;
-  //   this.shuffleCharacter();
-  //   this.checkGuess(name, this.updateTopScore);
-  // }
-
-  // shuffleCharacter = () => {
-  //   this.setState(this.state.characters = this.shuffleArray(this.state.characters))
-  // }
-
-  // shuffleArray = (a) => {
-  //   var j, x, i;
-  //   for (i = a.length - 1; i > 0; i--) {
-  //     j = Math.floor(Math.random() * (i + 1));
-  //     x = a[i];
-  //     a[i] = a[j];
-  //     a[j] = x;
-  //   }
-  //   return a;
-  // }
+  render() {
+    return(
+      <>
+      <h1>Friends</h1>
+      {this.state.friends.map(friend => (
+        <CharacterCard 
+        name={friend.name}
+        image={friend.image}
+        id={friend.id}
+        />
+      ))}
+      </>
+    );
+  };
 }
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-export default App;
+  export default App;
