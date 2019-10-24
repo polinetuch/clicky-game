6616a4f2ae73eee5f04f11a8bb9441f30054f250
Polinet <uchpolinet@gmail.com>
2019-10-24 23:32:15 +1100

    added images, id and name of all charactesr in characterCard.json file

--- clicky-game-app/src/App.js
+++ clicky-game-app/src/App.js
@@ -1,6 +1,62 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+
+class App extends Component {
+
+  state = {
+  };
+
+  render() {
+    return (<h1>hello world</h1>)
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // state = {
+  //   characters: characters,
+  //   pickedCharacter: [],
+  //   topScore: 0,
+  //   alertMessage: ""
+  // };
+
+  // handlePicked = event => {
+    
+  //   const name = event.target.attribute.getNamedItem("name").value;
+  //   this.shuffleCharacter();
+  //   this.checkGuess(name, this.updateTopScore);
+  // }
+
+  // shuffleCharacter = () => {
+  //   this.setState(this.state.characters = this.shuffleArray(this.state.characters))
+  // }
+
+  // shuffleArray = (a) => {
+  //   var j, x, i;
+  //   for (i = a.length - 1; i > 0; i--) {
+  //     j = Math.floor(Math.random() * (i + 1));
+  //     x = a[i];
+  //     a[i] = a[j];
+  //     a[j] = x;
+  //   }
+  //   return a;
+  // }
+}
 
 // function App() {
 //   return (
--- clicky-game-app/src/components/CharacterCard/CharacterCard.js
+++ clicky-game-app/src/components/CharacterCard/CharacterCard.js
@@ -1,33 +1,16 @@
 import React from 'react';
 import "./CharacterCard.css";
+import characters from "../characters.json";
 
 function CharacterCard(props) {
+    console.log(characters);
     return (
         <div className="card">
             <div className="img-container">
                 <img alt={props.name} src={props.image} />
             </div>
             <div className="content">
-                <ul>
-                    <li>
-                        <strong>Name</strong> {props.name[0]}
-                    </li>
-                    <li>
-                        <strong>Name:</strong> {props.name[1]}
-                    </li>
-                    <li>
-                        <strong>Name:</strong> {props.name[2]}
-                    </li>
-                    <li>
-                        <strong>Name</strong> {props.name[3]}
-                    </li>
-                    <li>
-                        <strong>Name:</strong> {props.name[4]}
-                    </li>
-                    <li>
-                        <strong>Name:</strong> {props.name[5]}
-                    </li>
-                </ul>
+                
             </div>
             <span onClick={() => props.shuffleCharacter(props.id)} className="shuffle"></span>
         </div>
