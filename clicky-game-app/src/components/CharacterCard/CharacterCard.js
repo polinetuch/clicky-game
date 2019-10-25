5e37684e1432fdfd65a3532f8ea6757d03077772
Polinet <uchpolinet@gmail.com>
2019-10-26 01:05:10 +1100

    added content for image card 'name'

--- clicky-game-app/src/components/CharacterCard/CharacterCard.js
+++ clicky-game-app/src/components/CharacterCard/CharacterCard.js
@@ -6,20 +6,14 @@ const CharacterCard = props => (
       <div className="img-container">
         <img alt={props.name} src={props.image} />
       </div>
+      <div className="content">
+        <ul>
+          <li>
+            <strong>Name:</strong> {props.name}
+          </li>
+        </ul>
+      </div>
     </div>
   );
-  
-
-// function CharacterCard(props) {
-//     return(
-//         <>
-//         <div className="card">
-//             <div className="img-container">
-//                 <img alt={props.name} src={props.image}/>
-//             </div>
-//         </div>
-//         </>
-//     );
-// }
 
 export default CharacterCard;
